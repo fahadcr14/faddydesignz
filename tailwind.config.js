bb9d1e22fa80e904536b20373a4618041d3382c8
fahadcr14 <bsdsf21m014@pucit.edu.pk>
2023-07-19 23:54:37 +0500

    v4.1

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,4 +12,23 @@ module.exports = {
   },
   darkMode: "class",
   plugins: [require("tw-elements/dist/plugin.cjs")],
+  safelist: [
+    'animate-[fade-in_1s_ease-in-out]',
+    'animate-[fade-in-down_1s_ease-in-out]',
+    'animate-[fade-in-up_1s_ease-in-out]',
+    'animate-[slide-in-right_1s_ease-in-out]',
+    'animate-[slide-in-left_1s_ease-in-out]',
+    'animate-[fade-in-left_1s_ease-in-out]',
+    'animate-[fade-in_1s_ease-in-out]',
+    'animate-[fade-in-right_1s_ease-in-out]',
+    'animate-[fade-in-left_1s_ease-in-out]',
+
+    'animate-[fade-out_1s_ease-in-out]',
+    'animate-[slide-left_1s_ease-in-out]',
+    'animate-[slide-right_1s_ease-in-out]',
+    'animate-[slide-left_1s_ease-in-out]',
+    'animate-[slide-up_1s_ease-in-out]',
+    'animate-[slide-down_1s_ease-in-out]',
+    // Add other animation classes here as needed
+  ],
 };
